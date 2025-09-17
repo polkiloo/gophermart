f74b8d877d9d6d6ff6b23ce3ea87612152825e02
Maikl Levitskiy <maikllevitskiy@192.168.1.5>
2025-09-17 23:19:39 +0300

    feat: support di container for app dependency.

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,3 +1,23 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"os/signal"
+	"syscall"
+
+	"go.uber.org/fx"
+
+	"github.com/polkiloo/gophermart/internal/di"
+)
+
+func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	app := fx.New(
+		fx.Provide(func() context.Context { return ctx }),
+		di.Module(),
+	)
+
+	run(ctx, app)
+}
